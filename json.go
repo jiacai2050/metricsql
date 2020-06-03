73a3105b454f1fadbf8b7881bcd5d827dd9815a1
jiacai2050 <jiacai2050@gmail.com>
2020-06-03 19:55:01 +0800

    add MarshalJSON for Expr

---
+++ json.go
@@ -0,0 +1,68 @@
+package metricsql
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+const (
+	HeadOp     = "op"
+	HeadFunc   = "func"
+	HeadAggr   = "aggr"
+	HeadRollup = "rollup"
+	HeadMetric = "metric"
+	HeadString = "string"
+	HeadNumber = "number"
+)
+
+func (e BinaryOpExpr) MarshalJSON() ([]byte, error) {
+	type wrap BinaryOpExpr
+	return wrapType(wrap(e), HeadOp)
+}
+
+func (e FuncExpr) MarshalJSON() ([]byte, error) {
+	type wrap FuncExpr
+	return wrapType(wrap(e), HeadFunc)
+}
+
+func (e AggrFuncExpr) MarshalJSON() ([]byte, error) {
+	type wrap AggrFuncExpr
+	return wrapType(wrap(e), HeadAggr)
+}
+
+func (e RollupExpr) MarshalJSON() ([]byte, error) {
+	type wrap RollupExpr
+	return wrapType(wrap(e), HeadRollup)
+}
+
+func (e MetricExpr) MarshalJSON() ([]byte, error) {
+	type wrap MetricExpr
+	return wrapType(wrap(e), HeadMetric)
+}
+
+func (e StringExpr) MarshalJSON() ([]byte, error) {
+	type wrap StringExpr
+	return wrapType(wrap(e), HeadString)
+}
+
+func (e NumberExpr) MarshalJSON() ([]byte, error) {
+	type wrap NumberExpr
+	return wrapType(wrap(e), HeadNumber)
+}
+
+func wrapType(v interface{}, typo string) ([]byte, error) {
+	var buf bytes.Buffer
+
+	buf.Write([]byte(fmt.Sprintf(`{"Head": "%s", "Body": `, typo)))
+	bs, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+
+	buf.Write(bs)
+	buf.WriteByte('}')
+
+	return buf.Bytes(), nil
+
+}
